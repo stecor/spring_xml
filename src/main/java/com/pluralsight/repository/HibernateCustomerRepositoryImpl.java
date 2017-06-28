04679a96fef03c58ea176d397f4c898cdca127f9
Stefano Corra <stefano.corra@hotmail.com>
2017-06-27 20:24:13 -0400

    fourth commit

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,15 +3,24 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
-	/* (non-Javadoc)
-	 * @see com.pluralsight.repository.CustomerRepository#findAll()
-	 */
+	@Value("${dbUsername}")
+	private String dbUsername;
+	
+	public void setDbUsername(String dbUsername) {
+		this.dbUsername = dbUsername;
+	}
+
 	@Override
 	public List<Customer> findAll(){
+		
+		System.out.println(dbUsername);
+		
 		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
